429e904271d54a550f88c863919558aee3a6fb53
spencerdallin <86256581+spencerdallin@users.noreply.github.com>
2021-12-02 16:01:20 -0800

    Add files via upload

---
+++ On_off_switch.ino
@@ -0,0 +1,31 @@
+
+//on off switch using GPIO on PB1 or digital pin 52
+
+//define port B pointers
+volatile unsigned char* port_b = (unsigned char*) 0x25;
+volatile unsigned char* ddr_b = (unsigned char*) 0x24;
+volatile unsigned char* pin_b = (unsigned char*) 0x23;
+
+//define button counter
+int i=0;
+void setup() {
+  //turn on pull up resistor
+  *port_b |= 0b11111111;
+
+  //set PB1 to input
+  *ddr_b &= 0b11111110;
+}
+
+void loop() {
+  if(*pin_b>0){
+    i++;
+  }
+ 
+   if(i%2==1){
+     //running
+    }
+    else{
+    //not running
+    }
+
+}
